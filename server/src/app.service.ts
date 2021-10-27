c0204f29126f6c98d9c342768b52490affe8308b
FreeDoM <jstrfaheem065@gmail.com>
2021-10-27 17:30:04 +0530

    Partial configration,

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Credentials, S3 } from 'aws-sdk';
+import awsConfig from './config';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  private access: Credentials;
+  private s3: S3;
+  constructor() {
+    this.access = new Credentials({
+      ...awsConfig,
+    });
+    this.s3 = new S3({});
+  }
+
+  createPresignedUrl(): string {
+    this.s3.getSignedUrl('putObject', {
+      Bucket: 's3udevstudy',
+      Key: '',
+      Expires: 100,
+    });
+    return 'Ok!';
+  }
+}
